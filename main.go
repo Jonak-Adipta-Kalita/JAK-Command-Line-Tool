a685eb485f69eef1435f0469202590e0ed1a5a49
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2023-09-09 10:45:05 +0530

    particullar really??

--- main.go
+++ main.go
@@ -13,82 +13,82 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "JAK Command Line Tool"
 	app.Description = "A Command Line Tool made by JAK!!"
-	
+
 	netFlag := []cli.Flag{
-		cli.StringFlag {
-			Name: "host",
+		cli.StringFlag{
+			Name:  "host",
 			Value: "jonakadiptakalita.tk",
 		},
 	}
-	
+
 	app.Commands = []cli.Command{
 		{
-			Name: "name-servers",
+			Name:  "name-servers",
 			Usage: "Looks up the Name Servers for a Perticular Host",
 			Flags: netFlag,
-			Action: func(ctx * cli.Context) error {
+			Action: func(ctx *cli.Context) error {
 				ns, err := net.LookupNS(ctx.String("host"))
 				if err != nil {
 					fmt.Println(err)
 					return err
 				}
-				
+
 				for i := 0; i < len(ns); i++ {
 					fmt.Println(ns[i].Host)
 				}
-				
+
 				return nil
 			},
 			Aliases: []string{"ns"},
 		},
 		{
-			Name: "ip",
+			Name:  "ip",
 			Usage: "Looks up the IPs for a particular host",
 			Flags: netFlag,
-			Action: func(ctx * cli.Context) error {
+			Action: func(ctx *cli.Context) error {
 				ip, err := net.LookupIP(ctx.String("host"))
 				if err != nil {
 					fmt.Println(err)
 					return err
 				}
-				
+
 				for i := 0; i < len(ip); i++ {
 					fmt.Println(ip[i])
 				}
-				
+
 				return nil
 			},
 		},
 		{
-			Name: "cname",
-			Usage: "Looks up the CNAME for a particullar host",
+			Name:  "cname",
+			Usage: "Looks up the CNAME for a particular host",
 			Flags: netFlag,
-			Action: func(ctx * cli.Context) error {
+			Action: func(ctx *cli.Context) error {
 				cname, err := net.LookupCNAME(ctx.String("host"))
 				if err != nil {
 					fmt.Println(err)
 					return err
 				}
-				
+
 				fmt.Println(cname)
 				return nil
 			},
 		},
 		{
-			Name: "mx",
-			Usage: "Looks up the MX Records for a particullar host",
+			Name:  "mx",
+			Usage: "Looks up the MX Records for a particular host",
 			Flags: netFlag,
-			Action: func(ctx * cli.Context) error {
+			Action: func(ctx *cli.Context) error {
 				mx, err := net.LookupMX(ctx.String("host"))
 				if err != nil {
 					fmt.Println(err)
 					return err
 				}
-				
+
 				for i := 0; i < len(mx); i++ {
 					fmt.Println(mx[i].Host, mx[i].Pref)
 				}
-				
+
 				return nil
 			},
 		},
